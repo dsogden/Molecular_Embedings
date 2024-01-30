b7303810562fc0790e60e92f4e3d074d341dac27
Dylan Ogden <67968474+dsogden@users.noreply.github.com>
2024-01-30 08:22:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ import numpy as np
 import pandas as pd
 import torch
 from tqdm import tqdm
+from utils import get_smiles, encode_smiles, train_step
 
 path = 'tox21.csv'
 df = pd.read_csv(path)
+smiles = get_smiles(df, 25)
 
-encoder = Encoder(len(smiles_encoder), 512, 128, 2)
-decoder = Decoder(len(smiles_encoder), 512, 128, len(smiles_encoder), 2)
+start_token = '<sos>'
+end_token = '<eos>'
+
+features, encoder_length = encode_smiles(smiles, 25, start_token, end_token)
+
+encoder = Encoder(encoder_length, 512, 128, 2)
+decoder = Decoder(encoder_length, 512, 128, encoder_length, 2)
 
 inputs, targets = torch.tensor(features, dtype=torch.int32), torch.tensor(features, dtype=torch.int32)
 N = inputs.shape[0] // 32
@@ -21,7 +28,7 @@ decoder_optimizer = torch.optim.Adam(decoder.parameters(), lr=1e-4)
 
 def main():
     indices = torch.arange(N)
-    permutation = torch.randperm(len(indices))
+    permutation = torch.randperm(indices.shape[0])
     indices = indices[permutation]
     epochs = 100
     
@@ -40,6 +47,8 @@ def main():
         training_loss.append([epoch + 1, loss])
         permutation = torch.randperm(len(indices))
         indices = indices[permutation]
+        
+     molecules = decode_smiles(inputs[-1], encoder, decoder)
 
 if __name__ == "__main__":
     main()
