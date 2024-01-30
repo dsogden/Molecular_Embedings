9e7b26e26e6e5076045726149c0f26401930c5bb
Dylan Ogden <67968474+dsogden@users.noreply.github.com>
2024-01-30 08:21:11 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import torch
 
 def get_smiles(dataframe, threshold):
     indices = []
@@ -27,3 +28,46 @@ def encode_smiles(smiles, max_length, start_token, end_token):
             features[idx, jdx] = smiles_encoder[letter]
         features[idx, length] = smiles_encoder[end_token]
     return features, len(smiles_encoder)
+
+def train_step(train_data, labels, indices, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion):
+    total_loss = 0
+    N = train_data.shape[0]
+    for idx in indices:
+        encoder_optimizer.zero_grad()
+        decoder_optimizer.zero_grad()
+
+        encoder_outputs, encoder_hidden, encoder_cell = encoder(train_data[idx])
+        decoder_outputs = decoder((encoder_hidden, encoder_cell), labels[idx])
+
+        loss = criterion(
+            decoder_outputs.view(-1, decoder_outputs.size(-1)),
+            labels[idx].view(-1).long()
+        )
+        loss.backward()
+
+        encoder_optimizer.step()
+        decoder_optimizer.step()
+
+        total_loss += loss.item()
+    return total_loss / N
+
+def decode_smiles(inputs, encoder, decoder):
+    batch_size = inputs.shape[0]
+    with torch.no_grad():
+        encoder_outputs, encoder_hidden = encoder(inputs)
+        decoder_outputs = decoder(encoder_hidden, None)
+    
+        _, topi = decoder_outputs.topk(1)
+        decoded_ids = topi.squeeze()
+    decoded_ids = decoded_ids.numpy()
+
+    decoded_smiles = []
+    for i in range(batch_size):
+        current_smile = ''
+        for idx in decoded_ids[i]:
+            if smiles_decoder[idx] == end_token:
+                break
+            else:
+                current_smile += smiles_decoder[idx]
+        decoded_smiles.append(current_smile)
+    return decoded_smiles
